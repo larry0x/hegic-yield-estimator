de33749b692cc300b6bf60a5c67f0d8894389d37
Larry <26318510+lyu18@users.noreply.github.com>
2020-11-09 15:56:45 -0600

    Fixed number formatting, etc.

--- js/main.js
+++ js/main.js
@@ -34,18 +34,16 @@ var POOL_SIZES = {
 var RESULT = {};
 
 // https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
-const _formatMoney = (number, decPlaces, decSep, thouSep) => {
-    decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces,
-    decSep = typeof decSep === "undefined" ? "." : decSep;
-    thouSep = typeof thouSep === "undefined" ? "," : thouSep;
-    var sign = number < 0 ? "-" : "";
+const _formatMoney = (number, decPlaces) => {
+    decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces;
+    var sign = number < 0 ? '-' : '';
     var i = String(parseInt(number = Math.abs(Number(number) || 0).toFixed(decPlaces)));
     var j = (j = i.length) > 3 ? j % 3 : 0;
 
     return sign +
-        (j ? i.substr(0, j) + thouSep : "") +
-        i.substr(j).replace(/(\decSep{3})(?=\decSep)/g, "$1" + thouSep) +
-        (decPlaces ? decSep + Math.abs(number - i).toFixed(decPlaces).slice(2) : "");
+        (j ? i.substr(0, j) + ',' : '') +
+        i.substr(j).replace(/\B(?=(\d{3})+(?!\d))/g, ",") +
+        (decPlaces ? '.' + Math.abs(number - i).toFixed(decPlaces).slice(2) : '');
 };
 
 const getCoinPrices = async () => {
